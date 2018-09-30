939322d68d9451289db5acae255ddeab2a45bb42
Linus Lee <100479lee@gmail.com>
2018-09-30 15:04:31 -0700

    Added contact page

--- static/js/main.js
+++ static/js/main.js
@@ -30,6 +30,10 @@ const validateBerkeleyEmail = address => {
     return address.match(/.+@berkeley.edu/) !== null;
 }
 
+const validateGenericEmail = address => {
+    return address.match(/.+@.+/) !== null;
+}
+
 const ifttt_request = (eventName, ...parameters) => {
     const queries = `value1=${parameters[0]}&value2=${parameters[1]}&value3=${parameters[2]}`;
     return fetch(`https://maker.ifttt.com/trigger/${eventName}/with/key/dd-ddOLbwB-c4dpX84bFd6?${queries}`, {
@@ -42,6 +46,22 @@ const ifttt_signup = (...parameters) => {
     return ifttt_request('studybuddycal_signup', ...parameters);
 }
 
+const ifttt_contact = (...parameters) => {
+    return ifttt_request('studybuddycal_contact', ...parameters);
+}
+
+const validateInputFields = validationDict => {
+    let allValid = true;
+    for (const [query, { func, message }] of Object.entries(validationDict)) {
+        if (!func(document.querySelector(query).value)) {
+            allValid = false;
+            alert(message);
+            break;
+        }
+    }
+    return allValid;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     for (const [query, phrases] of Object.entries(AB_TEST_REPLACEMENTS)) {
         if (document.querySelector(query)) {
@@ -51,6 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const signupButton = document.querySelector('.signupButton');
     const submitButton = document.querySelector('.submitButton');
+    const sendButton = document.querySelector('.sendButton');
 
     if (signupButton) {
         const signupInput = document.querySelector('.signupEmail');
@@ -71,7 +92,6 @@ document.addEventListener('DOMContentLoaded', () => {
             document.querySelector('[name=user_email').value = email;
         }
 
-        // attach event listeners
         const notEmpty = data => data.toString().trim() !== '';
         const VALIDATION = {
             'input[name=user_name]': {
@@ -88,19 +108,11 @@ document.addEventListener('DOMContentLoaded', () => {
             },
         };
 
+        // attach event listener
         submitButton.addEventListener('click', () => {
-            let allValid = true;
-            for (const [query, { func, message }] of Object.entries(VALIDATION)) {
-                if (!func(document.querySelector(query).value)) {
-                    allValid = false;
-                    alert(message);
-                    break;
-                }
-            }
+            if (validateInputFields(VALIDATION)) {
 
-            if (allValid) {
-
-                const parameters = {
+                const KEY_INPUT_NAME = {
                     name: 'user_name',
                     email: 'user_email',
                     class: 'user_class',
@@ -109,11 +121,11 @@ document.addEventListener('DOMContentLoaded', () => {
                     avail: 'user_availability',
                 };
 
-                const getval = key => {
-                    return document.querySelector(`[name=${parameters[key]}]`).value;
-                }
-
                 const parse = (tpl, ...parameters) => {
+                    const getval = key => {
+                        return document.querySelector(`[name=${KEY_INPUT_NAME[key]}]`).value.trim();
+                    }
+
                     let result = tpl[0];
                     let counter = 0;
                     for (const piece of tpl.slice(1)) {
@@ -123,7 +135,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
 
                 ifttt_signup(
-                    parse`${'name'} <${'email'}>`,
+                    parse`${'name'} (${'email'})`,
                     parse`${'class'}: ${'level'}`,
                     parse`${'topic'}, AVAIL ${'avail'}`
                 ).then(() => {
@@ -135,5 +147,63 @@ document.addEventListener('DOMContentLoaded', () => {
             }
 
         });
+    }
+
+    if (sendButton) {
+        // on the contacts page
+        const notEmpty = data => data.toString().trim() !== '';
+        const VALIDATION = {
+            'input[name=user_name]': {
+                func: notEmpty,
+                message: 'Please tell us your name!',
+            },
+            'input[name=user_email]': {
+                func: validateGenericEmail,
+                message: 'Did you correctly enter your email?'
+            },
+            'textarea[name=user_content]': {
+                func: notEmpty,
+                message: 'Did you mean to say something in the email?',
+            },
+        };
+
+        // attach event listener
+        sendButton.addEventListener('click', () => {
+            if (validateInputFields(VALIDATION)) {
+
+                const KEY_INPUT_NAME = {
+                    name: 'user_name',
+                    email: 'user_email',
+                    type: 'user_type',
+                    content: 'user_content',
+                };
+
+                const parse = (tpl, ...parameters) => {
+                    const getval = key => {
+                        return document.querySelector(`[name=${KEY_INPUT_NAME[key]}]`).value.trim();
+                    }
+
+                    let result = tpl[0];
+                    let counter = 0;
+                    for (const piece of tpl.slice(1)) {
+                        result += getval(parameters[counter++]) + piece;
+                    }
+                    return result;
+                }
+
+                ifttt_contact(
+                    parse`${'name'} (${'email'})`,
+                    parse`${'type'}`,
+                    parse`${'content'}`
+                ).then(() => {
+                    alert(parse`Thanks for reaching out, ${'name'}! We'll get back to you in the next few days.`);
+                    window.location.href = '/contact';
+                }).catch(e => {
+                    alert(`Looks like there was an error somewhere. Would you try again?\nError: ${e}`);
+                });
+            }
+
+        });
+
     }
 });
